e59fc92e247fdb95957a1ae22ed1fa70413baaf2
Jared-Thompkins <jaredthompkins21@gmail.com>
2023-01-14 00:07:47 -0600

    Initial Commit

---
+++ MemorizeGame/MemorizeGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeGameApp.swift
+//  MemorizeGame
+//
+//  Created by Jared Thompkins on 1/14/23.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
